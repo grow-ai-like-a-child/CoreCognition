9186aab3f7e49490cdf912c7ca76e24707d504e1
kaia Gao <kaiagao@kaias-MacBook-Pro.local>
2025-06-15 12:10:08 -0400

    update leaderboard results

--- convert.py
+++ convert.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import yaml
 
-df3 = pd.read_csv('/Users/kaiagao/Documents/top_total_models_by_concept.csv')
+for mode in ['image_only', 'single', 'total']:
+    df3 = pd.read_csv(f'/Users/kaiagao/Documents/top_{mode}_models_by_concept.csv') * 100
 
-# Convert to list of dictionaries
-records3 = df3.to_dict(orient='records')
+    # Convert to list of dictionaries
+    records3 = df3.to_dict(orient='records')
 
-# Save to YAML
-yaml_file_path3 = "_data/leaderboard_total.yml"
-with open(yaml_file_path3, 'w') as file:
-    yaml.dump(records3, file, sort_keys=False)
+    # Save to YAML
+    yaml_file_path3 = f"_data/leaderboard_{mode}.yml"
+    with open(yaml_file_path3, 'w') as file:
+        yaml.dump(records3, file, sort_keys=False)
