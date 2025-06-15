67d1f003c36fdcf53b722ec19c211dc9a46040a5
kaia Gao <kaiagao@kaias-MacBook-Pro.local>
2025-06-15 12:15:18 -0400

    update learderboard num;

--- convert.py
+++ convert.py
@@ -5,6 +5,7 @@ for mode in ['image_only', 'single', 'total']:
     df3 = pd.read_csv(f'/Users/kaiagao/Documents/top_{mode}_models_by_concept.csv')
     print(df3.columns)
     df3.loc[:, df3.columns[1:]] *= 100  # Scale columns from index 1 onwards by 100
+    df3.rename(columns={'true_mean': 'mean'}, inplace=True)
 
     # Convert to list of dictionaries
     records3 = df3.to_dict(orient='records')
